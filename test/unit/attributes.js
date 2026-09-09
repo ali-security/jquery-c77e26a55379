fe73bc8480183c737bf98446d7425320811e4bfb
Seal <info@sealsecurity.io>
2026-09-09 12:43:17 +0000

    CVE-2016-10707

--- test/unit/attributes.js
+++ test/unit/attributes.js
@@ -477,7 +477,9 @@ test( "attr(String, Object)", function() {
 test( "attr - extending the boolean attrHandle", function() {
 	expect( 1 );
 	var called = false,
-		_handle = jQuery.expr.attrHandle.checked || $.noop;
+		origAttrHandleHadChecked = "checked" in jQuery.expr.attrHandle,
+		origAttrHandleChecked = jQuery.expr.attrHandle.checked,
+		_handle = origAttrHandleChecked || $.noop;
 	jQuery.expr.attrHandle.checked = function() {
 		called = true;
 		_handle.apply( this, arguments );
@@ -486,6 +488,12 @@ test( "attr - extending the boolean attrHandle", function() {
 	called = false;
 	jQuery( "input" ).attr( "checked" );
 	ok( called, "The boolean attrHandle does not drop custom attrHandles" );
+
+	if ( origAttrHandleHadChecked ) {
+		jQuery.expr.attrHandle.checked = origAttrHandleChecked;
+	} else {
+		delete jQuery.expr.attrHandle.checked;
+	}
 });
 
 test( "attr(String, Object) - Loaded via XML document", function() {
@@ -1475,4 +1483,34 @@ test( "Insignificant white space returned for $(option).val() (#14858)", functio
 
 	val = jQuery( "<option>  test  </option>" ).val();
 	equal( val.length, 4, "insignificant white-space returned for value" );
+});
+
+test( "non-lowercase boolean attribute getters should not crash", function() {
+	expect( 6 );
+
+	// Two elements on purpose: jQuery.filter sends multi-element collections
+	// through Sizzle's own ATTR matcher, which hands the raw (mixed-case) name
+	// to the boolean attrHandle rather than the lowercased one
+	var elems = jQuery( "<input checked required autofocus type='checkbox'>" +
+		"<input checked required autofocus type='checkbox'>" );
+
+	jQuery.each({
+		checked: "Checked",
+		required: "requiRed",
+		autofocus: "AUTOFOCUS"
+	}, function( lowercased, original ) {
+		try {
+			strictEqual( elems.attr( original ), lowercased,
+				"The '" + original + "' attribute getter should return the lowercased name" );
+		} catch ( e ) {
+			ok( false, "The '" + original + "' attribute getter threw" );
+		}
+
+		try {
+			strictEqual( elems.filter( "[" + original + "]" ).length, 2,
+				"The '" + original + "' attribute selector should match without recursing" );
+		} catch ( e2 ) {
+			ok( false, "The '" + original + "' attribute selector threw" );
+		}
+	});
 });
